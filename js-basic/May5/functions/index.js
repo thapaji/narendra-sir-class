09cc0f47f9ca62001b8c58c76abc25f6490ae7c5
Sujan <mail.thapasujan@gmail.com>
2024-05-05 15:15:59 +1000

    function started

---
+++ js-basic/May5/functions/index.js
@@ -0,0 +1,8 @@
+function greeting(name) {
+    document.getElementById('output').innerHTML += `<h1>Hello ${name}</h1>`
+}
+
+greeting('World');
+greeting('Sujan');
+greeting('Class');
+greeting('Brothers');
